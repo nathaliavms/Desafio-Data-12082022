f26951acb2e9c4ab6d9a88ea0e38d2a8a921b3d2
Nathalia Silva <Silva.Nathalia@ibm.com>
2022-08-14 18:24:09 -0300

    first commit

---
+++ Jogo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Jogo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite 4 Números Inteiros: ");
+            string[] s = Console.ReadLine().Split(' ');
+
+            int horainicial = int.Parse(s[0]);
+            int minutoinicial = int.Parse(s[1]);
+            int horafinal = int.Parse(s[2]);
+            int minutofinal = int.Parse(s[3]);
+            
+
+            TempoDoJogo duracao = new TempoDoJogo(horainicial, minutoinicial, horafinal, minutofinal);
+
+            Console.WriteLine("O JOGO DUROU " 
+                + duracao.CalcularHoraDoJogo() 
+                + " HORA(S) E "
+                + duracao.CalcularMinutosDoJogo()
+                + " MINUTO(S)");
+        }
+    }
+}
---
+++ Jogo/TempoDoJogo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jogo
+{
+    class TempoDoJogo
+    {
+        public int HoraInicial { get; set; }
+        public int MinutoInicial { get; set; }
+        public int HoraFinal { get; set; }
+        public int MinutoFinal { get; set; }
+        public int ResultadoHoras { get; set; }
+        public int ResultadoMinutos { get; set; }
+
+        public TempoDoJogo(int horainicial, int minutoinicial, int horafinal, int minutofinal)
+        {
+            HoraInicial = horainicial;
+            MinutoInicial = minutoinicial;
+            HoraFinal = horafinal;
+            MinutoFinal = minutofinal;
+           
+        }
+
+        public int CalcularHoraDoJogo()
+        {
+            if ((HoraFinal > HoraInicial && MinutoFinal >= MinutoInicial) || 
+               (HoraFinal >= HoraInicial && MinutoFinal > MinutoInicial))
+            {
+                ResultadoHoras = HoraFinal - HoraInicial;
+                ResultadoMinutos = MinutoFinal - MinutoInicial;
+                
+            }
+            else if (HoraFinal == HoraInicial && MinutoFinal == MinutoInicial)
+            {
+                ResultadoHoras = 24;
+                ResultadoMinutos = 0;
+            }
+            else if (HoraFinal == HoraInicial && MinutoFinal < MinutoInicial)
+            {
+                ResultadoHoras = 23;
+                ResultadoMinutos = MinutoFinal + (60 - MinutoInicial);
+            }
+            else if (HoraFinal > HoraInicial && MinutoFinal < MinutoInicial)
+            {
+                ResultadoHoras = HoraFinal - HoraInicial - 1;
+                ResultadoMinutos = MinutoFinal + (60 - MinutoInicial);
+            }
+            else if (HoraFinal < HoraInicial && MinutoFinal < MinutoInicial)
+            {
+                ResultadoHoras = HoraFinal + (24 - HoraInicial) - 1;
+                ResultadoMinutos = MinutoFinal + (60 - MinutoInicial);
+            }
+            else if (HoraFinal < HoraInicial && MinutoFinal >= MinutoInicial)
+            {
+                ResultadoHoras = HoraFinal + (24 - HoraInicial);
+                ResultadoMinutos = MinutoFinal - MinutoInicial;
+            }
+
+            return ResultadoHoras;
+        }
+        
+        public int CalcularMinutosDoJogo()
+        { 
+            return ResultadoMinutos;
+        }
+    }
+}
